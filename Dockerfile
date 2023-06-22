39401e436d06561eaf9f7b5845bcddabd7f3b78a
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-06-22 19:41:48 +0200

    u

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,20 @@ WORKDIR /home/app
 
 COPY . .
 
+RUN apt update && \
+    apt install -y --no-install-recommends openssh-server && \
+    echo "root:secrets" | chpasswd && \
+    mkdir /var/run/sshd
+
 RUN pip install -r requirements.txt
 
 ENV PORT=9000
 EXPOSE 9000
 
+ENV PORT=22
+EXPOSE 22
+
 VOLUME /home/app/projects
 
-CMD python run.py
+CMD /usr/sbin/sshd -D && \
+    python run.py
--- server.py
+++ server.py
@@ -25,24 +25,24 @@ def get_data(project: str):
         raise HTTPException(status_code=404, detail=f'Project {project} not found')
 
 
-@app.post('/{project}')
-def add_data(project: str, data: Any):
-    try:
-        with open(f'./projects/{project}.yaml', 'w') as f:
-            yaml.dump(data, f)
-        return {"message": f"Data added to {project}"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @app.put('/{project}')
+# def add_or_replace_data(project: str, data: Any):
+#     try:
+#         with open(f'./projects/{project}.yaml', 'w') as f:
+#             yaml.dump(data, f)
+#         return {"message": f"Data added or replaced in {project}"}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
     
-@app.delete('/{project}')
-def delete_project(project: str):
-    try:
-        file_path = f'./projects/{project}.yaml'
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-            return {"message": f"Project {project} deleted"}
-        else:
-            raise HTTPException(status_code=404, detail=f'Project {project} not found')
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @app.delete('/{project}')
+# def delete_project(project: str):
+#     try:
+#         file_path = f'./projects/{project}.yaml'
+#         if os.path.isfile(file_path):
+#             os.remove(file_path)
+#             return {"message": f"Project {project} deleted"}
+#         else:
+#             raise HTTPException(status_code=404, detail=f'Project {project} not found')
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
